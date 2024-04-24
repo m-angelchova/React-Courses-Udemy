b12b4186bbb38c8c025ad6c665ac8d28ff7f03e0
m-angelchova <angelchova@gmail.com>
2024-04-24 18:09:25 +0300

    new project added, public vs src folder, updating state - based on previous state, managing user input

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); //must use a function to guarantee u have the latest available state (not setIsEditing(!isEditing))
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
+}
