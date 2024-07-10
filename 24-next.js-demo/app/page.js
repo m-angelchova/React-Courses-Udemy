1e6705d21df42cb1d76ab7143b2ef39714e994b0
m-angelchova <angelchova@gmail.com>
2024-07-10 14:31:56 +0300

    demo + init

---
+++ 24-next.js-demo/app/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About us</Link>
+      </p>
+      <p>
+        <Link href="/blog">Blog</Link>
+      </p>
+    </main>
+  );
+}
