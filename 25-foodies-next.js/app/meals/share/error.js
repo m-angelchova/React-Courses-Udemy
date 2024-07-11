415dd2bfe6b041b804126944fa6df71c50fb46ee
m-angelchova <angelchova@gmail.com>
2024-07-11 17:12:08 +0300

    updating shareMeal page and saving to db

---
+++ 25-foodies-next.js/app/meals/share/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Failed to Create Meal!</h1>
+      <p>Please enter valid input and try again.</p>
+    </main>
+  );
+}
