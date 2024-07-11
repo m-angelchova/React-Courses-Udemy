dd78a99dec9e1d5953eaffc862ad968b89d05b03
m-angelchova <angelchova@gmail.com>
2024-07-11 15:16:00 +0300

    meal detail page updated

---
+++ 25-foodies-next.js/app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Sorry, we could not find the meal you're looking for.</p>
+    </main>
+  );
+}
