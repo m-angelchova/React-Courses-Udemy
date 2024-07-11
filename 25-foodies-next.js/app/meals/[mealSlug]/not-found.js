e118b323879406f4629069fea3f059975b23dd0c
m-angelchova <angelchova@gmail.com>
2024-07-11 19:06:45 +0300

    revalidating path

--- 25-foodies-next.js/app/meals/[mealSlug]/not-found.js
+++ 25-foodies-next.js/app/meals/[mealSlug]/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Meal not Found</h1>
-      <p>Sorry, we could not find the meal you're looking for.</p>
+      <p>Sorry, we could not find the meal you`&apos;`re looking for.</p>
     </main>
   );
 }
--- 25-foodies-next.js/app/not-found.js
+++ 25-foodies-next.js/app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Not Found</h1>
-      <p>Sorry, we could not find the page you're looking for.</p>
+      <p>Sorry, we could not find the page you`&apos;`re looking for.</p>
     </main>
   );
 }
