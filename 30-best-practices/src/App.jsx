b086b2d1b28ccaed9961be51f725a011c59ddadc
m-angelchova <angelchova@gmail.com>
2024-07-16 13:48:29 +0300

    Render props

--- 30-best-practices/src/App.jsx
+++ 30-best-practices/src/App.jsx
@@ -1,11 +1,51 @@
 import Accordion from "./components/accordion/Accordion";
+import SearchableList from "./components/searchableList/SearchableList";
+import Place from "./components/Place.jsx";
+
+import savannaImg from "./assets/african-savanna.jpg";
+import amazonImg from "./assets/amazon-river.jpg";
+import caribbeanImg from "./assets/caribbean-beach.jpg";
+import desertImg from "./assets/desert-dunes.jpg";
+import forestImg from "./assets/forest-waterfall.jpg";
+
+const PLACES = [
+  {
+    id: "african-savanna",
+    image: savannaImg,
+    title: "African Savanna",
+    description: "Experience the beauty of nature.",
+  },
+  {
+    id: "amazon-river",
+    image: amazonImg,
+    title: "Amazon River",
+    description: "Get to know the largest river in the world.",
+  },
+  {
+    id: "caribbean-beach",
+    image: caribbeanImg,
+    title: "Caribbean Beach",
+    description: "Enjoy the sun and the beach.",
+  },
+  {
+    id: "desert-dunes",
+    image: desertImg,
+    title: "Desert Dunes",
+    description: "Discover the desert life.",
+  },
+  {
+    id: "forest-waterfall",
+    image: forestImg,
+    title: "Forest Waterfall",
+    description: "Listen to the sound of the water.",
+  },
+];
 
 function App() {
   return (
     <main>
       <section>
         <h2>Why work with us?</h2>
-
         <Accordion className="accordion">
           <Accordion.Item id="experience" className="accordion-item">
             <Accordion.Title className="accordion-item-title">
@@ -36,6 +76,17 @@ function App() {
             </Accordion.Content>
           </Accordion.Item>
         </Accordion>
+      </section>
+      <section>
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList
+          items={["item 1", "item 2", "item 3"]}
+          itemKeyFn={(item) => item}
+        >
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
