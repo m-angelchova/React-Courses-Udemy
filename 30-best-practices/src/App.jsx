10fd4e1bba36c9d99a4a2471f0b544cab518b481
m-angelchova <angelchova@gmail.com>
2024-07-16 13:13:07 +0300

    Compound components

--- 30-best-practices/src/App.jsx
+++ 30-best-practices/src/App.jsx
@@ -1,5 +1,44 @@
+import Accordion from "./components/accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+
+        <Accordion className="accordion">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We got 20 years of experience
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can&apos;t go wrong with us.</p>
+                <p>
+                  We are in the bussiness of planning highly individualized
+                  vacation trips for more than 20 years.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We're working with local guides
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>We are not doing this alone from our office.</p>
+                <p>
+                  Instea we are working with local guides to ensure a safe and
+                  pleasant vacation.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
