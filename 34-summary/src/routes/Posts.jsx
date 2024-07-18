1efcb12ab96db54e8b182fae349528dacccb8e89
m-angelchova <angelchova@gmail.com>
2024-07-18 17:15:52 +0300

    adding router

---
+++ 34-summary/src/routes/Posts.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import PostsList from "../components/PostsList";
+import { Outlet } from "react-router-dom";
+
+function Posts() {
+  return (
+    <>
+      <Outlet />
+      <main>
+        <PostsList />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
